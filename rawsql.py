79ccaacda9a92af677dede0f9437fb2693e4c9eb
Sh9hid <abdullah.cool1@gmail.com>
2024-06-01 14:46:09 +0530

    updated birds.db

---
+++ rawsql.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def connect_to_db(db_path):
+    print("Connecting... ")
+    return sqlite3.connect(db_path)
+
+if __name__ == "__main__":
+    with connect_to_db("birds.db") as connection:
+        connection.execute("""
+        CREATE TABLE IF NOT EXISTS bird (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        name TEXT NOT NULL
+        );
+""")
+    
